4595fafadc86dfef0e69910e8c9f6a70b6fe301f
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-04-29 21:32:30 +0200

    still on progress

---
+++ Main.java
@@ -0,0 +1,24 @@
+package snippet;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+import snippet.Gameplay;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		JFrame obj = new JFrame();
+		Gameplay gameplay= new Gameplay();
+		
+		obj.setBounds(10,10,905,700);
+		obj.setBackground(Color.DARK_GRAY);;
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gameplay);
+	}
+
+}
