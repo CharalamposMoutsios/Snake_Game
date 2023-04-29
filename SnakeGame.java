70f37214653f8b658eaa12c4e7de9cd81a50fa28
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-04-29 23:58:56 +0200

    updated

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,109 +1,80 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.ArrayList;
-import java.util.Random;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.Timer;
+public class SnakeGame extends JPanel implements ActionListener {
 
-public class SnakeGame implements ActionListener, KeyListener {
-    
-    public static final int WIDTH = 600;
-    public static final int HEIGHT = 600;
-    public static final int UNIT_SIZE = 20;
-    public static final int GAME_UNITS = (WIDTH * HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
-    public static final int DELAY = 75;
-    public static final int[] x = new int[GAME_UNITS];
-    public static final int[] y = new int[GAME_UNITS];
-    public static int bodyParts = 6;
-    public static int applesEaten = 0;
-    public static int appleX;
-    public static int appleY;
-    public static char direction = 'R';
-    public static boolean running = false;
-    public static Timer timer;
-    public static Random random;
-    public static JFrame frame;
-    public static JPanel panel;
-    
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 400;
+    private static final int DOT_SIZE = 10;
+    private static final int ALL_DOTS = 900;
+    private static final int RAND_POS = 29;
+    private static final int DELAY = 140;
+
+    private int[] x = new int[ALL_DOTS];
+    private int[] y = new int[ALL_DOTS];
+
+    private int dots;
+    private int apple_x;
+    private int apple_y;
+
+    private boolean leftDirection = false;
+    private boolean rightDirection = true;
+    private boolean upDirection = false;
+    private boolean downDirection = false;
+    private boolean inGame = true;
+
+    private Timer timer;
+    private Image ball;
+    private Image apple;
+    private Image head;
+
     public SnakeGame() {
-        frame = new JFrame("Snake Game");
-        frame.setSize(WIDTH, HEIGHT);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.addKeyListener(this);
-        
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        panel.setBackground(Color.black);
-        panel.setFocusable(true);
-        panel.addKeyListener(this);
-        frame.add(panel);
-        frame.setVisible(true);
-        
-        startGame();
+        addKeyListener(new TAdapter());
+        setBackground(Color.black);
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setFocusable(true);
+
+        ImageIcon iid = new ImageIcon("src/resources/dot.png");
+        ball = iid.getImage();
+
+        ImageIcon iia = new ImageIcon("src/resources/apple.png");
+        apple = iia.getImage();
+
+        ImageIcon iih = new ImageIcon("src/resources/head.png");
+        head = iih.getImage();
+
+        initGame();
     }
-    
-    public static void main(String[] args) {
-        new SnakeGame();
-    }
-    
-    public void startGame() {
-        random = new Random();
-        running = true;
-        direction = 'R';
-        generateApple();
+
+    private void initGame() {
+        dots = 3;
+
+        for (int z = 0; z < dots; z++) {
+            x[z] = 50 - z * 10;
+            y[z] = 50;
+        }
+
+        locateApple();
+
         timer = new Timer(DELAY, this);
         timer.start();
     }
-    
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        draw(g);
+
+    private void locateApple() {
+        int r = (int) (Math.random() * RAND_POS);
+        apple_x = r * DOT_SIZE;
+
+        r = (int) (Math.random() * RAND_POS);
+        apple_y = r * DOT_SIZE;
     }
-    
-    public void draw(Graphics g) {
-        if (running) {
-            for (int i = 0; i < HEIGHT / UNIT_SIZE; i++) {
-                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, HEIGHT);
-                g.drawLine(0, i * UNIT_SIZE, WIDTH, i * UNIT_SIZE);
-            }
-            
-            g.setColor(Color.red);
-            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-            
-            for (int i = 0; i < bodyParts; i++) {
-                if (i == 0) {
-                    g.setColor(Color.green);
-                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-                } else {
-                    g.setColor(new Color(45, 180, 0));
-                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-                }
-            }
-            
-            g.setColor(Color.red);
-            g.drawString("Score: " + applesEaten, 10, 20);
-        } else {
-            gameOver(g);
+
+    private void checkApple() {
+        if ((x[0] == apple_x) && (y[0] == apple_y)) {
+            dots++;
+            locateApple();
         }
     }
-    
-    public void generateApple() {
-        appleX = random.nextInt((int)(WIDTH / UNIT_SIZE)) * UNIT_SIZE;
-        appleY = random.nextInt((int)(HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
-    }
-    
-    public void move() {
-        for (int i = bodyParts; i > 0; i--) 
-        {x[i] = x[i - 1]};
-           
-                    
+
+    private void checkCollision() {
+        for (int z = dots; z > 0; z--) {
